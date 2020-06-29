f413ea00742480e27486ae715708f91550c810eb
badeshiben <badeshiben@gmail.com>
2020-06-29 09:12:08 -0600

    removed old dirs

--- BAR_ParametricReg/_RUN_ALL.bat
+++
@@ -1,6 +0,0 @@
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws05.0_reg0.5000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws05.0_reg2.7500.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws05.0_reg5.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws10.0_reg0.5000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws10.0_reg2.7500.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws10.0_reg5.0000.fst
--- Regularization_3/_RUN_ALL.bat
+++
@@ -1,8 +0,0 @@
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws04.0_CoreSpreadEddyVisc100.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws04.0_CoreSpreadEddyVisc500.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws04.0_CoreSpreadEddyVisc1000.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws04.0_CoreSpreadEddyVisc5000.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws12.0_CoreSpreadEddyVisc100.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws12.0_CoreSpreadEddyVisc500.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws12.0_CoreSpreadEddyVisc1000.0000.fst
-../../bin/openfast2.3-dev_x64s-vc-dbgout.exe ws12.0_CoreSpreadEddyVisc5000.0000.fst
