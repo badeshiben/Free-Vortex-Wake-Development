095e84d9e6cf612fee45d4bc35a93d296c4268c2
badeshiben <badeshiben@gmail.com>
2020-07-01 17:14:00 -0600

    reworked input dicts

--- BAR_02_template/OpenFAST_BAR_02.fst
+++ BAR_02_template/OpenFAST_BAR_02.fst
@@ -1,9 +1,9 @@
 ------- OpenFAST INPUT FILE -------------------------------------------
 Generated with AeroElasticSE FAST driver
 ---------------------- SIMULATION CONTROL --------------------------------------
-True                   Echo        - Echo input data to <RootName>.ech (flag)
+False                   Echo        - Echo input data to <RootName>.ech (flag)
 "FATAL"                AbortLevel  - Error level when simulation should abort (string) {"WARNING", "SEVERE", "FATAL"}
-30.0                   TMax        - Total run time (s)
+1400                 TMax        - Total run time (s)
 0.0005                   DT          - Recommended module time step (s)
 2                      InterpOrder - Interpolation order for input/output time history (-) {1=linear, 2=quadratic}
 0                      NumCrctn    - Number of correction iterations (-) {0=explicit calculation, i.e., no corrections}
@@ -13,7 +13,7 @@ True                   Echo        - Echo input data to <RootName>.ech (flag)
 2                      CompElast   - Compute structural dynamics (switch) {1=ElastoDyn; 2=ElastoDyn + BeamDyn for blades}
 1                      CompInflow  - Compute inflow wind velocities (switch) {0=still air; 1=InflowWind; 2=external from OpenFOAM}
 2                      CompAero    - Compute aerodynamic loads (switch) {0=None; 1=AeroDyn v14; 2=AeroDyn v15}
-1                      CompServo   - Compute control and electrical-drive dynamics (switch) {0=None; 1=ServoDyn}
+0                      CompServo   - Compute control and electrical-drive dynamics (switch) {0=None; 1=ServoDyn}
 0                      CompHydro   - Compute hydrodynamic loads (switch) {0=None; 1=HydroDyn}
 0                      CompSub     - Compute sub-structural dynamics (switch) {0=None; 1=SubDyn; 2=External Platform MCKF}
 0                      CompMooring - Compute mooring system (switch) {0=None; 1=MAP++; 2=FEAMooring; 3=MoorDyn; 4=OrcaFlex}
@@ -31,12 +31,12 @@ True                   Echo        - Echo input data to <RootName>.ech (flag)
 "unused"               MooringFile - Name of file containing mooring system input parameters (quoted string)
 "unused"               IceFile     - Name of file containing ice input parameters (quoted string)
 ---------------------- OUTPUT --------------------------------------------------
-True                   SumPrint    - Print summary data to "<RootName>.sum" (flag)
+False                   SumPrint    - Print summary data to "<RootName>.sum" (flag)
 5.0                    SttsTime    - Amount of time between screen status messages (s)
 99999.0                ChkptTime   - Amount of time between creating checkpoint files for potential restart (s)
-"default"              DT_Out      - Time step for tabular output (s) (or "default")
+0.05             DT_Out      - Time step for tabular output (s) (or "default")
 0.0                    TStart      - Time to begin tabular output (s)
-1                      OutFileFmt  - Format for tabular (time-marching) output file (switch) {1: text file [<RootName>.out], 2: binary file [<RootName>.outb], 3: both}
+2                      OutFileFmt  - Format for tabular (time-marching) output file (switch) {1: text file [<RootName>.out], 2: binary file [<RootName>.outb], 3: both}
 True                   TabDelim    - Use tab delimiters in text tabular output file? (flag) {uses spaces if false}
 "ES10.3E2"             OutFmt      - Format used for text tabular output, excluding the time channel.  Resulting field should be 10 characters. (quoted string)
 ---------------------- LINEARIZATION -------------------------------------------
--- create_studies.py
+++ create_studies.py
@@ -1,65 +1,113 @@
 import numpy as np
 
-""" study 1: DTfvw """
+""" DEFAULTS """
+dpsi_default = 10*np.pi/180  # rad
+wakeLengthD = 6
+rot_D = 102.996267808408*2 # m
+nearWakeExtent = 540*np.pi/180  # rad
 WS = [4, 6, 8, 10, 12]
 RPM = np.array([4, 5.5, 7.5, 7.84, 7.85])
 rotSpd = RPM * 0.104719755  # rad/s
-dpsi = np.array([2.5, 5, 7.5, 10, 12.5, 15]) * np.pi / 180  # rad
 Pitch = [0, 0, 0, 6, 10]  # deg
+DTfvw_default = np.round(dpsi_default/rotSpd, decimals=3) # sec
+nNWPanel_default = np.round(nearWakeExtent/(DTfvw_default * rotSpd), decimals=0)
+FWE = wakeLengthD * rot_D # m
+WakeLength_default = np.round(FWE * rotSpd/WS/dpsi_default, decimals=0)  # Npanels
+WakeRegFactor_default = 3
+WingRegFactor_default = 3
+CoreSpreadEddyVisc_default = 100
+
+DTfvw_cvg = np.ones(5)  # TODO
+dpsi_cvg = np.ones(5)  # TODO
+nNWPanel_cvg = np.ones(5)  # TODO
+nNWPanel_cvg = np.ones(5)  # TODO
+nNWPanel_cvg = np.ones(5)  # TODO
+WakeLength_cvg = np.ones(5)  # TODO
+WakeRegFactor_cvg = np.ones(5)  # TODO
+WingRegFactor_cvg = np.ones(5)  # TODO
+
+
+""" study 1: DTfvw """
+dpsi = np.array([2.5, 5, 7.5, 10, 12.5, 15]) * np.pi / 180  # rad
+
 DTfvw = np.round(np.outer(1/rotSpd, dpsi), decimals=3)
 study1 = {
-        'param' : 'DTfvw',
-        'WS'    : WS,
-        'RPM'   : RPM,
-        'pitch' : Pitch,
-        'values': DTfvw
+        'param'                         : 'DTfvw',
+        'WS'                            : WS,
+        'RPM'                           : RPM,
+        'pitch'                         : Pitch,
+        'DTfvw'                         : DTfvw,
+        'nNWPanel'                      : nNWPanel_default,
+        'WakeLength'                    : WakeLength_default,
+        'WakeRegFactor_default'         : WakeRegFactor_default * np.ones(len(WS)),
+        'WingRegFactor_default'         : WingRegFactor_default * np.ones(len(WS)),
+        'CoreSpreadEddyVisc_default'    : CoreSpreadEddyVisc_default * np.ones(len(WS))
     }
 
 """study 2: nNWpanel """
 NWE = np.array([30, 60, 120, 180, 240, 300, 360, 540, 720, 1080, 1440, 1800, 2160, 2520]) * np.pi / 180
-DTfvw = 0.2165*np.ones(5)  # TODO from previous study
-prod = DTfvw * rotSpd
-nNWPanel = np.round(np.outer(1/prod, NWE), decimals=3)
+prod = DTfvw_cvg * rotSpd
+nNWPanel = np.round(np.outer(1/prod, NWE), decimals=0)
 study2 = {
-        'param' : 'nNWPanel',
-        'WS'    : WS,
-        'RPM'   : RPM,
-        'pitch' : Pitch,
-        'values': nNWPanel
-    }
+        'param'                         : 'nNWPanel',
+        'WS'                            : WS,
+        'RPM'                           : RPM,
+        'pitch'                         : Pitch,
+        'DTfvw'                         : DTfvw_cvg,
+        'nNWPanel'                      : nNWPanel,
+        'WakeLength'                    : WakeLength_default,
+        'WakeRegFactor_default'         : WakeRegFactor_default * np.ones(len(WS)),
+        'WingRegFactor_default'         : WingRegFactor_default * np.ones(len(WS)),
+        'CoreSpreadEddyVisc_default'    : CoreSpreadEddyVisc_default * np.ones(len(WS))
+}
 
 """study 3: WakeLength"""
-FWE = np.array([231, 308, 385, 462, 539, 616, 693, 770, 847, 924])  # m
-dpsi = .1745*np.ones(5)  # TODO from previous study
-part = rotSpd / WS / dpsi
-WakeLength = np.round(np.outer(part, FWE), decimals=3)
+FWE = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) * rot_D  # m
+part = rotSpd / WS / dpsi_cvg
+WakeLength = np.round(np.outer(part, FWE), decimals=0)
 study3 = {
-        'param' : 'WakeLength',
-        'WS'    : WS,
-        'RPM'   : RPM,
-        'pitch' : Pitch,
-        'values': WakeLength
-    }
+        'param'                         : 'WakeLength',
+        'WS'                            : WS,
+        'RPM'                           : RPM,
+        'pitch'                         : Pitch,
+        'DTfvw'                         : DTfvw_cvg,
+        'nNWPanel'                      : nNWPanel_cvg,
+        'WakeLength'                    : WakeLength,
+        'WakeRegFactor_default'         : WakeRegFactor_default * np.ones(len(WS)),
+        'WingRegFactor_default'         : WingRegFactor_default * np.ones(len(WS)),
+        'CoreSpreadEddyVisc_default'    : CoreSpreadEddyVisc_default * np.ones(len(WS))
+}
 
 """study 4: WakeRegFactor"""
 WakeRegFactor = np.outer(np.ones(len(WS)), np.array([0, 0.1, 0.5, 1, 1.5, 2, 2.5, 3, 5]))
 study4 = {
-        'param' : 'WakeRegFactor',
-        'WS'    : WS,
-        'RPM'   : RPM,
-        'pitch' : Pitch,
-        'values': WakeRegFactor
-    }
+        'param'                         : 'WakeRegFactor',
+        'WS'                            : WS,
+        'RPM'                           : RPM,
+        'pitch'                         : Pitch,
+        'DTfvw'                         : DTfvw_cvg,
+        'nNWPanel'                      : nNWPanel_cvg,
+        'WakeLength'                    : WakeLength_cvg,
+        'WakeRegFactor_default'         : WakeRegFactor,
+        'WingRegFactor_default'         : WingRegFactor_default * np.ones(len(WS)),
+        'CoreSpreadEddyVisc_default'    : CoreSpreadEddyVisc_default * np.ones(len(WS))
+}
+
 
 """study 5: WingRegFactor"""
 WingRegFactor = np.outer(np.ones(len(WS)), np.array([0, 0.1, 0.5, 1, 1.5, 2, 2.5, 3, 5]))
 study5 = {
-        'param' : 'WingRegFactor',
-        'WS'    : WS,
-        'RPM'   : RPM,
-        'pitch' : Pitch,
-        'values': WingRegFactor
-    }
+        'param'                         : 'WingRegFactor',
+        'WS'                            : WS,
+        'RPM'                           : RPM,
+        'pitch'                         : Pitch,
+        'DTfvw'                         : DTfvw_cvg,
+        'nNWPanel'                      : nNWPanel_cvg,
+        'WakeLength'                    : WakeLength_cvg,
+        'WakeRegFactor_default'         : WakeRegFactor_cvg,
+        'WingRegFactor_default'         : WingRegFactor,
+        'CoreSpreadEddyVisc_default'    : CoreSpreadEddyVisc_default * np.ones(len(WS))
+}
 """study 6: """
 WS = [4, 12]
 RPM = np.array([4, 7.85])
@@ -67,9 +115,16 @@ rotSpd = RPM * 0.104719755  # rad/s
 Pitch = [0, 10]  # deg
 CoreSpreadEddyVisc = np.outer(np.ones(len(WS)), np.array([100, 500, 1000, 5000]))
 study6 = {
-        'param' : 'CoreSpreadEddyVisc',
-        'WS'    : WS,
-        'RPM'   : RPM,
-        'pitch' : Pitch,
-        'values': CoreSpreadEddyVisc
-    }
+        'param'                         : 'CoreSpreadEddyVisc',
+        'WS'                            : WS,
+        'RPM'                           : RPM,
+        'pitch'                         : Pitch,
+        'DTfvw'                         : DTfvw_cvg[[1, -1]],
+        'nNWPanel'                      : nNWPanel_cvg[[1, -1]],
+        'WakeLength'                    : WakeLength_cvg[[1, -1]],
+        'WakeRegFactor_default'         : WakeRegFactor_cvg[[1, -1]],
+        'WingRegFactor_default'         : WingRegFactor_cvg[[1, -1]],
+        'CoreSpreadEddyVisc_default'    : CoreSpreadEddyVisc
+}
+m=0
+n = m+1
