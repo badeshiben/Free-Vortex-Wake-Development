7a00b7022b3f1cbd006dea88dcddd1ec987a38af
badeshiben <badeshiben@gmail.com>
2020-07-10 12:04:58 -0600

    changed FAST outputs in ED and AD. Started on blade-span output plot fxns. Modded ;other output plot fxns.

--- FVW_PostProOLD.py
+++ FVW_PostProOLD.py
@@ -26,21 +26,15 @@ def resolution_raw(varying, WS, plot):
     varying:    parameter varied in study
     WS:         list of wind speeds [m/s]
     plot:       plot options. [0, 1, 2] - [no plot, show plot, save plot]
-    outputs:    list of fastlib df output names [str]
 
     Returns
     -------
     plots results vs resolution
 
     """
-    # outputs = ['OoPDefl1_[m]', 'IPDefl1_[m]', 'TTDspFA_[m]', 'RootMxb1_[kN-m]', 'RootMyb1_[kN-m]', 'RootMzb1_[kN-m]',\
-    #            'RotTorq_[kN-m]', 'LSSGagMya_[kN-m]', 'LSSGagMza_[kN-m]', 'YawBrMxp_[kN-m]', 'YawBrMyp_[kN-m]',\
-    #            'YawBrMzp_[kN-m]', 'TwrBsMxt_[kN-m]', 'TwrBsMyt_[kN-m]', 'TwrBsMzt_[kN-m]', 'GenPwr_[kW]', 'RotTorq_[kN-m]']
-    # nplots = len(outputs)
-    fig, ax = plt.subplots(6, 3, sharey=False, sharex = True, figsize=(15, 20))  # (6.4,4.8)
+    fig, ax = plt.subplots(6, 3, sharey=False, sharex = True, figsize=(10, 10))  # (6.4,4.8)
     for ws in WS:
         df = pd.read_csv('Results_ws{:04.1f}.csv'.format(ws),sep='\t')
-        oo = df[['OoPDefl1_[m]', 'IPDefl1_[m]']]
         """ Deflections"""
         ax[0, 0].set_ylabel('Deflection [m]')
         ax[0, 0].plot(df[varying], df['OoPDefl1_[m]'], 'o-',  label='WS = {:}'.format(ws))
@@ -79,6 +73,7 @@ def resolution_raw(varying, WS, plot):
         ax[4, 0].text(.5, .5, 'TwrBsMxt', transform=ax[4, 0].transAxes)
         ax[4, 1].plot(df[varying], df['TwrBsMyt_[kN-m]'], 'o-',  label='WS = {:}'.format(ws))
         ax[4, 1].text(.5, .5, 'TwrBsMyt', transform=ax[4, 1].transAxes)
+        ax[4, 1].set_xlabel(varying)
         ax[4, 2].plot(df[varying], df['TwrBsMzt_[kN-m]'], 'o-', label='WS = {:}'.format(ws))
         ax[4, 2].text(.5, .5, 'TwrBsMzt', transform=ax[4, 2].transAxes)
         ax[4, 2].set_xlabel(varying)
@@ -87,13 +82,9 @@ def resolution_raw(varying, WS, plot):
         ax[5, 0].plot(df[varying], df['GenPwr_[kW]'], 'o-',  label='WS = {:}'.format(ws))
         ax[5, 0].text(.5, .5, 'GenPwr', transform=ax[5, 0].transAxes)
         ax[5, 0].set_xlabel(varying)
-        """Generator torque"""
-        ax[5, 1].set_ylabel('Torque [kN-m]')
-        ax[5, 1].plot(df[varying], df['GenTq_[kN-m]'], 'o-',  label='WS = {:}'.format(ws))
-        ax[5, 1].text(.5, .5, 'GenTq', transform=ax[5, 1].transAxes)
-        ax[5, 1].set_xlabel(varying)
     plt.delaxes()
-    ax[5, 1].legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.delaxes()
+    ax[5, 0].legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.tick_params(direction='in')
     if plot == 1:
         plt.show()
@@ -117,7 +108,7 @@ def resolution_pDiff(varying, WS, plot):
     plots of % diff between results at current and finest resolution
 
     """
-    fig, ax = plt.subplots(6, 3, sharey=False, sharex=True, figsize=(15, 20))  # (6.4,4.8)
+    fig, ax = plt.subplots(6, 3, sharey=False, sharex=True, figsize=(10, 10))  # (6.4,4.8)
     for ws in WS:
         df = pd.read_csv('Results_ws{:04.1f}.csv'.format(ws), sep='\t')
         n = len(df[varying])
@@ -164,6 +155,7 @@ def resolution_pDiff(varying, WS, plot):
         ax[4, 0].text(.5, .5, 'TwrBsMxt', transform=ax[4, 0].transAxes)
         ax[4, 1].plot(df[varying], dfpdiff['TwrBsMyt_[kN-m]'], 'o-', label='WS = {:}'.format(ws))
         ax[4, 1].text(.5, .5, 'TwrBsMyt', transform=ax[4, 1].transAxes)
+        ax[4, 1].set_xlabel(varying)
         ax[4, 2].plot(df[varying], dfpdiff['TwrBsMzt_[kN-m]'], 'o-', label='WS = {:}'.format(ws))
         ax[4, 2].text(.5, .5, 'TwrBsMzt', transform=ax[4, 2].transAxes)
         ax[4, 2].set_xlabel(varying)
@@ -172,13 +164,9 @@ def resolution_pDiff(varying, WS, plot):
         ax[5, 0].plot(df[varying], dfpdiff['GenPwr_[kW]'], 'o-', label='WS = {:}'.format(ws))
         ax[5, 0].text(.5, .5, 'GenPwr', transform=ax[5, 0].transAxes)
         ax[5, 0].set_xlabel(varying)
-        """Generator torque"""
-        ax[5, 1].set_ylabel('Torque [kN-m]')
-        ax[5, 1].plot(df[varying], df['GenTq_[kN-m]'], 'o-',  label='WS = {:}'.format(ws))
-        ax[5, 1].text(.5, .5, 'GenTq', transform=ax[5, 1].transAxes)
-        ax[5, 1].set_xlabel(varying)
     plt.delaxes()
-    ax[5, 1].legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.delaxes()
+    ax[5, 0].legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.tick_params(direction='in')
     if plot == 1:
         plt.show()
@@ -194,49 +182,13 @@ def spanwise_values(varying, WS, plot):
     Parameters
     ----------
     varying:    parameter varied in study
-    WS:         single chosen wind speeds [m/s]
+    WS:         list of wind speeds [m/s]
     plot:       plot options. [0, 1, 2] - [no plot, show plot, save plot]
 
     Returns
     -------
     plots outputs along blade span
     """
-
-    fig, ax = plt.subplots(4, 2, sharey=False, sharex=True, figsize=(15, 20))
-    norm_node_r = np.linspace(0, 1, 9)
-    df = pd.read_csv('Results_ws{:04.1f}'.format(WS) + '_' + varying + '.csv', sep='\t')
-    AxInd = df[['B1N1AxInd', 'B1N2AxInd', 'B1N3AxInd', 'B1N4AxInd', 'B1N5AxInd', 'B1N6AxInd', 'B1N7AxInd', 'B1N8AxInd', 'B1N9AxInd']]
-    TnInd = df[['B1N1TnInd', 'B1N2TnInd', 'B1N3TnInd', 'B1N4TnInd', 'B1N5TnInd', 'B1N6TnInd', 'B1N7TnInd', 'B1N8TnInd', 'B1N9TnInd']]
-    Fn = df[['B1N1Fn', 'B1N2Fn', 'B1N3Fn', 'B1N4Fn', 'B1N5Fn', 'B1N6Fn', 'B1N7Fn', 'B1N8Fn', 'B1N9Fn']]
-    Ft = df[['B1N1Ft', 'B1N2Ft', 'B1N3Ft', 'B1N4Ft', 'B1N5Ft', 'B1N6Ft', 'B1N7Ft', 'B1N8Ft', 'B1N9Ft']]
-    Fl = df[['B1N1Fl', 'B1N2Fl', 'B1N3Fl', 'B1N4Fl', 'B1N5Fl', 'B1N6Fl', 'B1N7Fl', 'B1N8Fl', 'B1N9Fl']]
-    Fd = df[['B1N1Fd', 'B1N2Fd', 'B1N3Fd', 'B1N4Fd', 'B1N5Fd', 'B1N6Fd', 'B1N7Fd', 'B1N8Fd', 'B1N9Fd']]
-    Circ = df[['Gam1B1', 'Gam2B1', 'Gam3B1', 'Gam4B1', 'Gam5B1', 'Gam6B1', 'Gam7B1', 'Gam8B1', 'Gam9B1']]
-
-    ax[0, 0].set_ylabel('Axial Induction')
-    ax[0, 0].plot(norm_node_r, AxInd, 'o-', label='WS = {:}'.format(WS))
-    ax[0, 1].set_ylabel('Tangential Induction')
-    ax[0, 1].plot(norm_node_r, TnInd, 'o-', label='WS = {:}'.format(WS))
-    ax[1, 0].set_ylabel('Normal Force [N]')
-    ax[1, 0].plot(norm_node_r, Fn, 'o-', label='WS = {:}'.format(WS))
-    ax[1, 1].set_ylabel('Tangential Force [N]')
-    ax[1, 1].plot(norm_node_r, Ft, 'o-', label='WS = {:}'.format(WS))
-    ax[2, 0].set_ylabel('Lift Force [N]')
-    ax[2, 0].plot(norm_node_r, Fl, 'o-', label='WS = {:}'.format(WS))
-    ax[2, 1].set_ylabel('Drag Force [N]')
-    ax[2, 1].plot(norm_node_r, Fd, 'o-', label='WS = {:}'.format(WS))
-    ax[3, 0].set_ylabel('Circulation')
-    ax[3, 0].plot(norm_node_r, Fl, 'o-', label='WS = {:}'.format(WS))
-
-    ax[3, 0].legend(loc='upper left', bbox_to_anchor=(1, 1))
-    plt.tick_params(direction='in')
-
-    if plot == 1:
-        plt.show()
-        plt.close()
-    elif plot == 2:
-        plot_name = "Figures/" + 'SPANWISE_' + varying + "_ws{:04.1f}".format(WS) + ".pdf"
-        plt.savefig(plot_name, bbox_inches='tight')
 
 
 """ RUN RESOLUTION STUDY """
@@ -252,27 +204,30 @@ def run_study(WS, name, values):
     -------
     plots of % diff between results at current and finest resolution
     """
-    work_dir = 'SampleOutputs'
+    work_dir = 'BAR_02_discretization_inputs/' + study['param'] + '/' # Output folder
     for wsp in WS:
         i = WS.index(wsp)
         outFiles=[]
         for val in values:
-            case     ='ws{:04.1f}_reg{:.4f}'.format(wsp, val)
-            filename = os.path.join(work_dir, case + '.outb')
+            case     ='ws{:.0f}_'.format(wsp)+study['param']+'{:.3f}'.format(val[i])
+            filename = os.path.join(work_dir, case + '.out')
             outFiles.append(filename)
         dfAvg = fastlib.averagePostPro(outFiles,avgMethod='periods',avgParam=1,ColMap={'WS_[m/s]':'Wind1VelX_[m/s]'})
-        dfAvg.insert(0,name, values)
+        dfAvg.insert(0,study['param'], values)
         # --- Save to csv since step above can be expensive
         dfAvg.to_csv('Results_ws{:04.1f}_'.format(wsp) + name + '.csv', sep='\t', index=False)
         #print(dfAvg)
-    resolution_raw(name, WS, 2)
-    resolution_pDiff(name, WS, 2)
-    spanwise_values(name, WS, 2)
+    resolution_raw(study['param'], WS, 2)
+    resolution_pDiff(study['param'], WS, 2)
+    spanwise_values(study['param'], WS, 2)
     print('Ran ' + name + ' post processing')
 
 
 if __name__ == "__main__":
-    run_study(WS=[5, 10], name='Reg_[m]', values=[.5, 2.75, 5])
+    studies = [study1, study2, study3, study4, study5, study6]
+
+    for study in studies:
+        run_study(study['WS'], study['param'], study['values'])
 
 
 
